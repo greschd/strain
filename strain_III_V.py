62c8e279bee6220cc0c3d935c921f9932f90e945
Dominik Gresch <greschd@gmx.ch>
2017-03-09 14:44:53 +0100

    merged strain_III_V and material_parameters

--- strain_III_V.py
+++ strain_III_V.py
@@ -74,3 +74,7 @@ class Uniaxial110(CartesianStrain):
             deformation_matrix=deformation_matrix,
             pos_displacement_matrices=pos_displacement_matrices
         )
+
+InAs = StrainParameters_III_V(C11=8.329, C12=4.526, C44=3.959, zeta=0.58)
+GaSb = StrainParameters_III_V(C11=8.834, C12=4.023, C44=4.322, zeta=0.99)
+InSb = StrainParameters_III_V(C11=6.918, C12=3.788, C44=3.132, zeta=0.90)
