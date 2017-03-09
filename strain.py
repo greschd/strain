a404f3cf1737f4e21ca61bee5338429995137366
Dominik Gresch <greschd@gmx.ch>
2017-03-09 14:40:31 +0100

    fixed the way the strength multiplier works in CartesianStrain

--- strain.py
+++ strain.py
@@ -18,18 +18,21 @@ class Strain(six.with_metaclass(abc.ABCMeta, object)):
 
 class CartesianStrain(Strain):
     def __init__(self, deformation_matrix, pos_displacement_matrices=None):
-        self.deformation = Deformation(np.eye(3) + deformation_matrix)
+        self.deformation_matrix = deformation_matrix
         self.pos_displacement_matrices = pos_displacement_matrices
 
     def apply(structure, strength_multiplier=1.):
-        new_structure = self.deformation.apply_to_structure(structure)
+        deformation = Deformation(
+            np.eye(3) + strength_multiplier * self.deformation_matrix
+        )
+        new_structure = deformation.apply_to_structure(structure)
         # move positions
         if self.pos_displacement_matrices is not None:
             for idx, mat in self.pos_displacement_matrices:
                 new_structure.translate_sites(
                     indices=[idx],
                     # use original cartesian positions
-                    vector=np.dot(mat, struc.cart_coords[idx]),
+                    vector=strength_multiplier * np.dot(mat, struc.cart_coords[idx]),
                     frac_coords=False
                 )
         return new_structure
